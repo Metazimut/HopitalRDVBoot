c313bd4f6f9c8e2b84f0321c382a2c0d45aee360
Mathieu W <w.mathieu.dk59@gmail.com>
2021-08-18 16:38:13 +0200

    admin

--- src/main/java/sopra/formation/rest/AdminController.java
+++ src/main/java/sopra/formation/rest/AdminController.java
@@ -1,73 +1,95 @@
 package sopra.formation.rest;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.fasterxml.jackson.annotation.JsonView;
 
 import sopra.formation.model.Admin;
+import sopra.formation.model.Views;
 import sopra.formation.repository.ICompteRepository;
 
-@Controller
+@RestController
+@RequestMapping("/admin")
+@CrossOrigin("*")
 public class AdminController {
 
 	@Autowired
 	private ICompteRepository adminRepo;
 
 	@GetMapping("/admin") // ETAPE 1
-	public String list(Model model) {
-		// ETAPE 2
-		List<Admin> admins = this.adminRepo.findAllAdmin();
-
-		// ETAPE 3
-		model.addAttribute("mesAdmins", admins);
-
-		// ETAPE 4
-		return "admin/list";
+	public List<Admin> findAll() {
+		return adminRepo.findAllAdmin();
 	}
 
-	@GetMapping("/admin/add")
-	public String add() {
-		// ETAPE 2 et 3 inutile
-		// ETAPE 4
-		return "admin/form";
-	}
+	@GetMapping("/{id}")
+	@JsonView(Views.ViewAdmin.class)
+	public Admin find(@PathVariable Long id) {
 
-	@GetMapping("/admin/edit")
-	public String edit(@RequestParam("id") Long idAdmin, Model model) {
-		// ETAPE 2
-		Admin admin = adminRepo.findAdminById(idAdmin);
+		Admin optAdmin = adminRepo.findAdminById(id);
 
-		// ETAPE 3
-		model.addAttribute("prat", admin);
-
-		// ETAPE 4
-		return "admin/form";
+		if (optAdmin!=null) {
+			return optAdmin;
+		} else {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find resource");
+		}
 	}
 	
-	@PostMapping("/admin/save")
-	public String save(@RequestParam(required = false) Long id, @RequestParam(required = false, defaultValue = "0") int version, @RequestParam String nom, @RequestParam String prenom, @RequestParam String email, @RequestParam String mdp) {
-		Admin admin = new Admin(id,version, nom, prenom, email, mdp);
-		admin.setVersion(version);
-		
-		adminRepo.save(admin);
-		
-		return "redirect:/admin";
+	@GetMapping("/{id}/detail")
+	@JsonView(Views.ViewAdminDetail.class)
+	public Admin detail(@PathVariable Long id) {
+
+		Admin optAdmin = adminRepo.findAdminById(id);
+
+		if (optAdmin!=null) {
+			return optAdmin;
+		} else {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find resource");
+		}
 	}
 
-	@GetMapping("/admin/delete")
-	public String delete(@RequestParam Long id) {
+	@PostMapping("")
+	@JsonView(Views.ViewAdmin.class)
+	public Admin create(@RequestBody Admin stagiaire) {
+		stagiaire = adminRepo.save(stagiaire);
+
+		return stagiaire;
+	}
+
+	@PutMapping("/{id}")
+	@JsonView(Views.ViewAdmin.class)
+	public Admin update(@RequestBody Admin stagiaire, @PathVariable Long id) {
+		if (!adminRepo.existsById(id)) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find resource");
+		}
+
+		stagiaire = adminRepo.save(stagiaire);
+
+		return stagiaire;
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		if (!adminRepo.existsById(id)) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find resource");
+		}
+		
 		adminRepo.deleteById(id);
-		
-		return "redirect:/admin";
-	}
-	
-	@GetMapping("/admin/cancel")
-	public String cancel() {
-		return "forward:/admin";
 	}
 }
